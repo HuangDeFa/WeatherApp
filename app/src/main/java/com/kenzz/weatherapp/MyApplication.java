d8ad876985ebbd5cb4d8e3a295272c87e46e9e2c
huangdefa <huangdefa@kenpro.local>
2017-08-10 00:30:01 +0800

    Init project

---
+++ app/src/main/java/com/kenzz/weatherapp/MyApplication.java
@@ -0,0 +1,14 @@
+package com.kenzz.weatherapp;
+
+import android.app.Application;
+
+/**
+ * Created by huangdefa on 10/08/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
